9ee2031fb39da899064c788d1d53ee3783cbc66e
Giovani Silva <giovani-silva1@live.com>
2021-05-31 12:06:12 -0300

    criado o projeto e inserido as dependencias iniciais, criado a primeira migracao de Categoria utilizando o conceito do flywayDB

---
+++ src/test/java/com/fullstackangular/api/FullstackAngularSpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackangular.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAngularSpringBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
